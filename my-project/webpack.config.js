b2c387cbc3ce168555338a7a121bef4eb2a9d985
xuanyi0627 <xuanyicode@icloud.com>
2021-07-11 22:42:26 +0800

    feat: 添加babel-loader

--- my-project/webpack.config.js
+++ my-project/webpack.config.js
@@ -22,5 +22,13 @@ module.exports = {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js'
     },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
     mode: 'production'
 }
