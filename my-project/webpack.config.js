85f8f6fb4884eda8e04dccaf39c2f08b1039de90
xuanyi0627 <xuanyicode@icloud.com>
2021-07-18 22:44:37 +0800

    feat: 自动清理构建目录产物

--- my-project/webpack.config.js
+++ my-project/webpack.config.js
@@ -1,6 +1,7 @@
 'use strict'
 const path = require('path')
 const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 // 单入口
 module.exports = {
@@ -20,63 +21,13 @@ module.exports = {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    mode: 'development',
+    mode: 'production',
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin()
     ],
     devServer: {
         contentBase: './dist',
         hot: true
     }
 }
-
-// 多入口
-// module.exports = {
-//     // 默认false，也就是不开启
-//     watch: true,
-//     // 只有开启监听模式，watchOptions才有意义
-//     watchOptions: {
-//         // 默认为空，不监听的文件或者文件夹，支持正则匹配
-//         ignored: /node_modules/,
-//         // 监听到变化后会等300ms再去执行，默认300ms
-//         aggregateTimeout: 300,
-//         // 判断文件是否发生变化，是通过不停询问系统指定文件有没有变化实现的，默认每秒问1000次
-//         poll: 1000
-//     },
-//     entry: {
-//         index: './src/index.js',
-//         search: './src/search.js'
-//     },
-//     output: {
-//         path: path.join(__dirname, 'dist'),
-//         filename: '[name].js'
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /.js$/,
-//                 use: 'babel-loader'
-//             },
-//             {
-//                 test: /.less$/,
-//                 use: [
-//                     'style-loader',
-//                     'css-loader',
-//                     'less-loader'
-//                 ]
-//             },
-//             {
-//                 test: /.(png|svg|jpg|jpeg|gif)$/,
-//                 use: 'url-loader',
-//                 options: {
-//                     limit: 10240
-//                 }
-//             },
-//             {
-//                 test: /.(woff|woff2|eot|ttf|otf)$/,
-//                 use: 'file-loader'
-//             }
-//         ]
-//     },
-//     mode: 'development'
-// }
