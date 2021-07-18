31c71020bbbf75d5c96abe08b514b1b5daf01441
xuanyi0627 <xuanyicode@icloud.com>
2021-07-18 22:21:51 +0800

    feat: 文件压缩

--- my-project/webpack.config.js
+++ my-project/webpack.config.js
@@ -1,36 +1,12 @@
 'use strict'
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 // 单入口
-module.exports = {
-    // 默认false，也就是不开启
-    watch: true,
-    // 只有开启监听模式，watchOptions才有意义
-    watchOptions: {
-        // 默认为空，不监听的文件或者文件夹，支持正则匹配
-        ignored: /node_modules/,
-        // 监听到变化后会等300ms再去执行，默认300ms
-        aggregateTimeout: 300,
-        // 判断文件是否发生变化，是通过不停询问系统指定文件有没有变化实现的，默认每秒问1000次
-        poll: 1000
-    },
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    }
-}
-
-// 多入口
 // module.exports = {
 //     // 默认false，也就是不开启
 //     watch: true,
@@ -43,40 +19,117 @@ module.exports = {
 //         // 判断文件是否发生变化，是通过不停询问系统指定文件有没有变化实现的，默认每秒问1000次
 //         poll: 1000
 //     },
-//     entry: {
-//         index: './src/index.js',
-//         search: './src/search.js'
-//     },
+//     entry: './src/index.js',
 //     output: {
 //         path: path.join(__dirname, 'dist'),
-//         filename: '[name].js'
+//         filename: 'bundle.js'
 //     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /.js$/,
-//                 use: 'babel-loader'
-//             },
-//             {
-//                 test: /.less$/,
-//                 use: [
-//                     'style-loader',
-//                     'css-loader',
-//                     'less-loader'
-//                 ]
-//             },
-//             {
-//                 test: /.(png|svg|jpg|jpeg|gif)$/,
-//                 use: 'url-loader',
-//                 options: {
-//                     limit: 10240
-//                 }
-//             },
-//             {
-//                 test: /.(woff|woff2|eot|ttf|otf)$/,
-//                 use: 'file-loader'
-//             }
-//         ]
-//     },
-//     mode: 'development'
+//     mode: 'production',
 // }
+
+// 多入口
+module.exports = {
+    // 默认false，也就是不开启
+    watch: true,
+    // 只有开启监听模式，watchOptions才有意义
+    watchOptions: {
+        // 默认为空，不监听的文件或者文件夹，支持正则匹配
+        ignored: /node_modules/,
+        // 监听到变化后会等300ms再去执行，默认300ms
+        aggregateTimeout: 300,
+        // 判断文件是否发生变化，是通过不停询问系统指定文件有没有变化实现的，默认每秒问1000次
+        poll: 1000
+    },
+    entry: {
+        index: './src/index.js',
+        search: './src/search.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name]_[chunkhash:8].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name]_[hash:8].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name]_[hash:8].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name]_[contenthash].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/search.html'),
+            filename: 'search.html',
+            chunks: ['search'],
+            inject: true,
+            minify: {
+                html5: true,
+                collapseInlineTagWhitespace: true,
+                preserveLineBreaks: false,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            chunks: ['index'],
+            inject: true,
+            minify: {
+                html5: true,
+                collapseInlineTagWhitespace: true,
+                preserveLineBreaks: false,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: false
+            }
+        })
+    ],
+    mode: 'production'
+}
